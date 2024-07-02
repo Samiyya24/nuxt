3bfd481209745977da564c97e2f9315bf00882ca
Samiyya24 <samiyyaazizkhanova904@gmail.com>
2024-07-02 07:33:23 +0500

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div>
+    <h>About page</h>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div>
+    <h>Home page</h>
+    <nuxt-link to="/about">About</nuxt-link>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
